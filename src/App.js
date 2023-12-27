81295a12126214d8c1bf5e2a22cd4b4081b254f1
Kartikey Jaiswal <karan74jaiswal@gmail.com>
2023-12-27 18:48:11 +0530

    made a picture selling website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [pictures, setPictures] = useState([
+    { id: 1, src: "image1.jpg", price: 10 },
+    { id: 2, src: "image2.jpg", price: 15 },
+    { id: 3, src: "image3.jpg", price: 20 },
+    { id: 4, src: "image4.jpg", price: 25 },
+  ]);
+  const [cartItems, setCartItems] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const handleBuyClick = (pictureId) => {
+    const selectedPicture = pictures.find(
+      (picture) => picture.id === pictureId
+    );
+    if (selectedPicture) {
+      const newCartItems = cartItems.concat(selectedPicture);
+      const remainingPictures = pictures.filter(
+        (picture) => picture.id !== pictureId
+      ); // Filter out purchased picture
+      setCartItems(newCartItems);
+      setPictures(remainingPictures);
+      setTotalPrice((prevTotalPrice) => prevTotalPrice + selectedPicture.price);
+    } else {
+      console.error("Picture not found:", pictureId);
+    }
+  };
+  const handleRemoveFromCart = (pictureId) => {
+    const pictureToRemove = cartItems.find((item) => item.id === pictureId);
+
+    if (pictureToRemove) {
+      setCartItems(cartItems.filter((item) => item.id !== pictureId));
+      setTotalPrice((prevTotalPrice) => prevTotalPrice - pictureToRemove.price);
+
+      // Add removed picture to pictures state
+      setPictures((pictures) => pictures.concat(pictureToRemove));
+    } else {
+      console.warn("Picture not found in cart:", pictureId);
+    }
+  };
+
+  const handlePayClick = () => {
+    console.log("Payment successful!");
+    setPictures(
+      pictures.filter(
+        (picture) => !cartItems.find((item) => item.id === picture.id)
+      )
+    );
+    setCartItems([]);
+    setTotalPrice(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <PictureList pictures={pictures} handleBuyClick={handleBuyClick} />
+      <ShoppingCart
+        cartItems={cartItems}
+        totalPrice={totalPrice}
+        handlePayClick={handlePayClick}
+        handleRemoveFromCart={handleRemoveFromCart}
+      />
+    </div>
+  );
+}
+
+function PictureList({ pictures, handleBuyClick }) {
+  return (
+    <div>
+      <h2>Available Pictures</h2>
+      <div className="picture-grid">
+        {pictures.map((picture) => (
+          <div key={picture.id} className="picture-item">
+            <img src={picture.src} alt={picture.id} />
+            <p>Price: ${picture.price}</p>
+            <button onClick={() => handleBuyClick(picture.id)}>Buy</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function ShoppingCart({
+  cartItems,
+  totalPrice,
+  handlePayClick,
+  handleRemoveFromCart,
+}) {
+  return (
+    <div>
+      <h2>Shopping Cart</h2>
+      <div className="cart-items">
+        {cartItems.length > 0 ? (
+          cartItems.map((item) => (
+            <div key={item.id} className="cart-item">
+              <img src={item.src} alt={item.id} />
+              <p>Price: ${item.price}</p>
+              <button onClick={() => handleRemoveFromCart(item.id)}>
+                Remove
+              </button>
+            </div>
+          ))
+        ) : (
+          <p>Your cart is empty.</p>
+        )}
+      </div>
+      <p className="cart-total">Total Price: ${totalPrice}</p>
+      <button onClick={handlePayClick}>Pay</button>
     </div>
   );
 }
